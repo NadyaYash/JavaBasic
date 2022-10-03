a3b60eccec64e784a184caaac0e5a21baf045c2c
NadyaYash <rabotakvn@gmail.com>
2022-10-03 19:33:12 +0200

    Create HomeTask8.java

---
+++ HomeTask8.java
@@ -0,0 +1,25 @@
+/**
+ * Java Basic HomeTask8
+ *
+ * @author Nadzeya Yashchuk
+ * @todo 28.09.2022
+ * @date 03.10.2022
+ */
+public class HomeTask8 {
+    public static void main(String[] args) {
+        Cat cat = new Cat("Barsik", "red", 2, 1);
+        System.out.println("Cat: " + cat.name + ", color: " + cat.color + ", age: " + cat.age);
+        System.out.println("Try to play: " + cat.play());
+        cat.feed();
+        System.out.println("Try to play: " + cat.play());
+        System.out.println("Try to play: " + cat.play());
+        cat.feed();
+        System.out.println("Try to jump: " + cat.jump(1));
+        cat.feed();
+        System.out.println("Try to jump: " + cat.jump(2));
+
+        Cat cat1 = new Cat("Murzik", "gray", 4, 1 );
+        System.out.println(cat1);
+        System.out.println(cat1.voice());
+    }
+}
